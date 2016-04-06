1988f2444289e898d13a4307aceb4fafcc66bedb
dnalwqer <liuweiqiang1992@gmail.com>
2016-04-05 23:39:55 -0400

    gallery

--- MyRuns2/src/main/java/edu/dartmouth/cs/actiontabs/MainActivity.java
+++ MyRuns2/src/main/java/edu/dartmouth/cs/actiontabs/MainActivity.java
@@ -4,10 +4,6 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -46,53 +42,5 @@ public class MainActivity extends Activity {
         // make sure the tabs are equally spaced.
         slidingTabLayout.setDistributeEvenly(true);
         slidingTabLayout.setViewPager(viewPager);
-
-
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case R.id.menuitem_search:
-			Toast.makeText(this, getString(R.string.ui_menu_search),
-					Toast.LENGTH_SHORT).show();
-			return true;
-		case R.id.menuitem_send:
-			Toast.makeText(this, getString(R.string.ui_menu_send),
-					Toast.LENGTH_SHORT).show();
-			return true;
-		case R.id.menuitem_add:
-			Toast.makeText(this, getString(R.string.ui_menu_add),
-					Toast.LENGTH_SHORT).show();
-			return true;
-		case R.id.menuitem_share:
-			Toast.makeText(this, getString(R.string.ui_menu_share),
-					Toast.LENGTH_SHORT).show();
-			return true;
-		case R.id.menuitem_feedback:
-			Toast.makeText(this, getString(R.string.ui_menu_feedback),
-					Toast.LENGTH_SHORT).show();
-			return true;
-		case R.id.menuitem_about:
-			Toast.makeText(this, getString(R.string.ui_menu_about),
-					Toast.LENGTH_SHORT).show();
-			return true;
-		case R.id.menuitem_quit:
-			Toast.makeText(this, getString(R.string.ui_menu_quit),
-					Toast.LENGTH_SHORT).show();
-			finish(); // close the activity
-			return true;
-		}
-		return false;
-	}
-
-
-
 }
